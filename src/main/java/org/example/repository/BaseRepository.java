ec3261221844a80abbb11b4e8e47196ac12ca285
Kristina M <kristina0x7@gmail.com>
2025-12-16 01:00:29 +0100

    Create abstract class BaseRepository with constructor and generics

--- src/main/java/org/example/repository/BaseRepository.java
+++ src/main/java/org/example/repository/BaseRepository.java
@@ -1,4 +1,14 @@
 package org.example.repository;
 
-public class BaseRepository {
+import jakarta.persistence.EntityManagerFactory;
+
+public abstract class BaseRepository <T, ID> {
+
+    private final EntityManagerFactory emf;
+    protected final Class <T> entityClass;
+
+    protected BaseRepository(EntityManagerFactory emf, Class<T> entityClass) {
+        this.emf = emf;
+        this.entityClass = entityClass;
+    }
 }
