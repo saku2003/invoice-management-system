56bf9901a8ba5b63a8630f95e539e8ccd727e739
Annika Holmqvist <annika.holmqvist94@gmail.com>
2025-12-17 09:55:14 +0100

    Extend `InvoiceRepository` from `BaseRepository` and implement `createInvoice` method

--- src/main/java/org/example/repository/InvoiceRepository.java
+++ src/main/java/org/example/repository/InvoiceRepository.java
@@ -1,4 +1,20 @@
 package org.example.repository;
 
-public class InvoiceRepository {
+import jakarta.persistence.EntityManagerFactory;
+import org.example.entity.Invoice;
+
+import java.util.UUID;
+
+public class InvoiceRepository extends BaseRepository<Invoice, UUID>{
+    protected InvoiceRepository(EntityManagerFactory emf) {
+        super(emf, Invoice.class);
+    }
+
+    public Invoice createInvoice(Invoice invoice) {
+        return runInTransaction(em -> {
+            em.persist(invoice);
+            return invoice;
+        });
+
+    }
 }
