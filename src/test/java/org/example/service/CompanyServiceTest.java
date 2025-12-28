70d3dbc41aca9ba9ce4672af75ff7d9cea8d9ea6
Kristina M <kristina0x7@gmail.com>
2025-12-28 19:10:17 +0100

    Set up mocks and instantiate CompanyService before each test

--- src/test/java/org/example/service/CompanyServiceTest.java
+++ src/test/java/org/example/service/CompanyServiceTest.java
@@ -3,7 +3,10 @@ package org.example.service;
 import org.example.repository.CompanyRepository;
 import org.example.repository.CompanyUserRepository;
 import org.example.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
 
 class CompanyServiceTest {
 
@@ -11,6 +14,14 @@ class CompanyServiceTest {
     private CompanyUserRepository companyUserRepository;
     private CompanyRepository companyRepository;
     private CompanyService companyService;
+
+    @BeforeEach
+    void setUp() {
+        userRepository = mock(UserRepository.class);
+        companyUserRepository = mock(CompanyUserRepository.class);
+        companyRepository = mock(CompanyRepository.class);
+        companyService = new CompanyService(companyRepository, companyUserRepository, userRepository);
+    }
 
     @Test
     void create() {
