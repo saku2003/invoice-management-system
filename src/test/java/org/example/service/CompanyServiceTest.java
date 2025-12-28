20eaab01e86d8227d19d846aba9c6a69728bef5d
Kristina M <kristina0x7@gmail.com>
2025-12-28 19:08:33 +0100

    add fields for repositories and CompanyService in test class

--- src/test/java/org/example/service/CompanyServiceTest.java
+++ src/test/java/org/example/service/CompanyServiceTest.java
@@ -1,10 +1,16 @@
 package org.example.service;
 
+import org.example.repository.CompanyRepository;
+import org.example.repository.CompanyUserRepository;
+import org.example.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class CompanyServiceTest {
+
+    private UserRepository userRepository;
+    private CompanyUserRepository companyUserRepository;
+    private CompanyRepository companyRepository;
+    private CompanyService companyService;
 
     @Test
     void create() {
